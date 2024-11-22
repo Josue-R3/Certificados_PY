51aaba48d52b6b80b9d54dd73189e0e0c4080937
Josue Ruiz <106292389+Josue-R3@users.noreply.github.com>
2024-11-22 09:29:46 -0500

    cambio de plantilla y endpoint

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from typing import List
 from PIL import Image, ImageDraw, ImageFont
 from barcode import Code128
 from barcode.writer import ImageWriter
-import aiohttp
 import io
 import os
 from azure.storage.blob import BlobServiceClient, ContentSettings
@@ -17,10 +16,9 @@ import time
 app = FastAPI()
 
 # Configuración general
-TEMPLATE_PATH = "template/plantilla2.png"
+TEMPLATE_PATH = "template/plantilla.png"
 FONT_BOLD_PATH = "font/Poppins-Bold.ttf"
 FONT_REGULAR_PATH = "font/Poppins-Regular.ttf"
-FECHA_SUFRA = "03 de Diciembre del 2024"
 BLOB_ACCOUNT_URL = "https://votacionesalmacenamiento.blob.core.windows.net/"
 CONTAINER_NAME = "certificados"
 SAS_TOKEN = "sp=racwl&st=2024-11-16T22:36:04Z&se=2025-11-17T23:00:00Z&spr=https&sv=2022-11-02&sr=c&sig=yrGG5iaE7vwIwK3f5WDsd8obOP6aNyLKt%2FktAUBKEMY%3D"
@@ -29,9 +27,14 @@ SAS_TOKEN = "sp=racwl&st=2024-11-16T22:36:04Z&se=2025-11-17T23:00:00Z&spr=https&
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(message)s")
 logger = logging.getLogger("CertificateAPI")
 
-# Deshabilitar logs del SKD de Azure
+# Deshabilitar logs del SDK de Azure
 azure_logger = logging.getLogger("azure")
 azure_logger.setLevel(logging.WARNING)
+
+# Variables de caché
+template_image = None
+font_bold = None
+font_regular = None
 
 # Modelo de datos
 class CertificateData(BaseModel):
@@ -42,25 +45,45 @@ class CertificateData(BaseModel):
     rol: str
 
 
-# Descargar plantilla desde la URL
-async def fetch_image_from_url(url: str) -> Image:
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            if response.status != 200:
-                raise HTTPException(status_code=500, detail="Error al descargar la plantilla.")
-            content = await response.read()
-            return Image.open(io.BytesIO(content))
+# Cargar recursos al inicio
+def load_resources():
+    global template_image, font_bold, font_regular
+
+    # Verificar plantilla
+    if not os.path.exists(TEMPLATE_PATH):
+        raise RuntimeError(f"No se encontró la plantilla en la ruta: {TEMPLATE_PATH}")
+    try:
+        template_image = Image.open(TEMPLATE_PATH)
+        logger.info("Plantilla cargada correctamente.")
+    except Exception as e:
+        raise RuntimeError(f"Error al cargar la plantilla: {e}")
+
+    # Verificar fuentes
+    if not os.path.exists(FONT_BOLD_PATH):
+        raise RuntimeError(f"No se encontró la fuente en negrita en la ruta: {FONT_BOLD_PATH}")
+    if not os.path.exists(FONT_REGULAR_PATH):
+        raise RuntimeError(f"No se encontró la fuente regular en la ruta: {FONT_REGULAR_PATH}")
+    try:
+        font_bold = ImageFont.truetype(FONT_BOLD_PATH, 60)
+        font_regular = ImageFont.truetype(FONT_REGULAR_PATH, 60)
+        logger.info("Fuentes cargadas correctamente.")
+    except Exception as e:
+        raise RuntimeError(f"Error al cargar las fuentes: {e}")
 
 
 # Generar código de barras
 def generate_barcode(data: str) -> Image:
-    writer = ImageWriter()
-    barcode_instance = Code128(data, writer=writer)
-    buffer = io.BytesIO()
-    options = {'write_text': False}
-    barcode_instance.write(buffer, options)
-    buffer.seek(0)
-    return Image.open(buffer)
+    try:
+        writer = ImageWriter()
+        barcode_instance = Code128(data, writer=writer)
+        buffer = io.BytesIO()
+        options = {'write_text': False}
+        barcode_instance.write(buffer, options)
+        buffer.seek(0)
+        barcode_image = Image.open(buffer)
+        return barcode_image.rotate(90, expand=True) # Rotar el codigo de barras 90 grados
+    except Exception as e:
+        raise RuntimeError(f"Error al generar el código de barras: {e}")
 
 
 # Subir imagen al Blob Storage
@@ -77,35 +100,34 @@ async def upload_to_azure_blob(image_data: bytes, blob_name: str) -> str:
 
 
 # Procesar un certificado individual
-async def process_certificate(data: CertificateData, template: Image, font_bold, font_regular) -> dict:
+async def process_certificate(data: CertificateData) -> dict:
     try:
         start_time = time.time()
         logger.info(f"Iniciando proceso para identificación: {data.identificacion}")
 
         # Copiar plantilla
-        template_copy = template.copy()
+        template_copy = template_image.copy()
         draw = ImageDraw.Draw(template_copy)
 
         # Coordenadas de textos generales
         text_positions = {
-            "numeracion": (1600, 245),
-            "nombre": (90, 500),
-            "identificacion": (90, 710),
-            "fecha": (90, 1220),
-            "rol": (1480, 1000),
+            "numeracion": (1650, 137),
+            "nombre": (70, 460),
+            "identificacion": (70, 680),
+            "rol": (1260, 900),
         }
 
         # Dibujar textos generales
         for key, position in text_positions.items():
-            text = FECHA_SUFRA if key == "fecha" else getattr(data, key)
+            text = getattr(data, key)
             font = font_regular if key not in ['numeracion', 'rol'] else font_bold
-            color = "black" if key not in ['numeracion', 'rol'] else "#3185D6"
+            color = "#000000" if key not in ['numeracion', 'rol'] else "#3185D6"
             draw.text(position, text, font=font, fill=color)
 
         # Manejo del texto "carrera"
         carrera = data.carrera
-        carrera_primary_position = (90, 925)
-        carrera_secondary_position = (90, 1000)
+        carrera_primary_position = (70, 880)
+        carrera_secondary_position = (70, 970)
 
         max_width = 900
         lines = []
@@ -126,8 +148,8 @@ async def process_certificate(data: CertificateData, template: Image, font_bold,
             draw.text(carrera_secondary_position, lines[1], font=font_regular, fill="black")
 
         # Generar código de barras
-        barcode_image = generate_barcode(data.identificacion).resize((450, 150))
-        barcode_position = (1400, 1100)
+        barcode_image = generate_barcode(data.identificacion).resize((150, 1300))
+        barcode_position = (1830, 210)
         template_copy.paste(barcode_image, barcode_position)
 
         logger.info(f"Certificado generado para identificación: {data.identificacion} en {time.time() - start_time:.2f} segundos")
@@ -156,19 +178,21 @@ async def process_certificate(data: CertificateData, template: Image, font_bold,
 async def generate_certificates(data_list: List[CertificateData]):
     try:
         logger.info(f"Procesando un lote de {len(data_list)} certificados.")
-
-        # Descargar plantilla y fuentes
-        template = await fetch_image_from_url(TEMPLATE_PATH)
-        font_bold = ImageFont.truetype(FONT_BOLD_PATH, 50)
-        font_regular = ImageFont.truetype(FONT_REGULAR_PATH, 50)
-
-        # Procesar cada certificado concurrentemente
-        tasks = [process_certificate(data, template, font_bold, font_regular) for data in data_list]
+        tasks = [process_certificate(data) for data in data_list]
         results = await asyncio.gather(*tasks)
-
         logger.info("Proceso de generación y subida completado.")
         return JSONResponse(content={"results": results}, status_code=200)
 
     except Exception as e:
         logger.error(f"Error general en el proceso: {e}")
         raise HTTPException(status_code=500, detail=str(e))
+
+
+# Evento de inicio para cargar recursos
+@app.on_event("startup")
+def on_startup():
+    try:
+        load_resources()
+    except RuntimeError as e:
+        logger.error(f"Error al cargar recursos: {e}")
+        raise RuntimeError(f"Error al cargar recursos: {e}")
